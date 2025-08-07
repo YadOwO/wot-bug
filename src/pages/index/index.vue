93573b83414ffed79247ba0d094fe3b309a05d4a
余俊杰 <yujunjie@xingwennetwork.com>
2025-08-07 21:41:08 +0800

    feat: 【蒙层 bug】复现

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -39,6 +39,9 @@ function openUrl(url: string) {
 
 <template>
   <view class="box-border py-3">
+    <!-- 这里是演示 -->
+    <wd-picker :columns="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]" label="bug 演示" />
+
     <view class="mx-3 box-border rounded-3 bg-white px-4 py-6 text-center dark:bg-[var(--wot-dark-background2)]">
       <text class="mb-3 block text-left text-5 text-gray-800 font-bold dark:text-gray-200">
         Wot Demo
